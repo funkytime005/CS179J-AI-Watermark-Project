26b0beb6fa7d45194e77d08653f15136a174370f
augustpbennett <augustpbennennet@gmail.com>
2024-02-29 15:37:57 -0800

    updated apply.py to replace test pixel alteration (prev. blackening) with readable, virtually imperceptible value tweaks

--- apply.py
+++ apply.py
@@ -1,17 +1,32 @@
-import glob, os
+import os
 from PIL import Image
 
-def apply_watermark(fileName):
-    img = Image.open(fileName)
-    # img.load()
+def set_value(channel): # sets individual color channel value within one pixel
+    return round(channel / 10) * 10 # divides channel value by 10, rounds to nearest whole number, then appends 0. effectively rounds channel value to nearest 10
+
+def apply_watermark(fileName): # applies watermark pattern to entire image
+    img = Image.open(fileName) # open passed filename
+    _img = img.load()
     [width, height] = img.size
 
-    for x in range(width):
+    for x in range(width): # parse over image
         for y in range(height):
-            if (x % 10 == 0) and (y % 10 == 0): # watermark pattern (decide on this w group)
-                img.putpixel((x, y), (0, 0, 0)) # TODO: current test method blackens pixel, set values to tweak rgb values specifically
-    img.show()
-
+            if (x % 10 == 0) and (y % 10 == 0): # watermark pattern: edit every 10th pixel on x, y (figure out something with unlikely FP rate)
+                [r, g, b] = _img[x, y] # get triplet of rgb values in current pixel
+                
+                r = set_value(r) # watermark pixel
+                g = set_value(g)
+                b = set_value(b)
+                
+                '''
+                print('red channel: ' + str(r) + '\n' + # test print: ensure that each color channel has had its last digit removed
+                      'green channel: ' + str(g) + '\n' + 
+                      'blue channel: ' + str(b) + '\n')
+                '''
+                
+                img.putpixel((x, y), (r, g, b)) # set pixel value to watermarked pixel value
+                #img.putpixel((x, y), (0, 0, 0)) # TODO: current test method blackens pixel, set values to tweak rgb values specifically
+    img.show() # show image
 def main():
     fileExists = False
     while not fileExists:
