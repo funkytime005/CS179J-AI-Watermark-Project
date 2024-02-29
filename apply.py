fac7babfcb5ae897610aff0311d150aef26f3315
augustpbennett <augustpbennennet@gmail.com>
2024-02-29 15:47:59 -0800

    set_value() revision to prevent returning rgb values higher than expected

--- apply.py
+++ apply.py
@@ -2,7 +2,8 @@ import os
 from PIL import Image
 
 def set_value(channel): # sets individual color channel value within one pixel
-    return round(channel / 10) * 10 # divides channel value by 10, rounds to nearest whole number, then appends 0. effectively rounds channel value to nearest 10
+    return min((round(channel / 10) * 10), 255) # divides channel value by 10, rounds to nearest whole number, then appends 0. effectively rounds channel value to nearest 10.
+                                                # if rounded value > 255 (max value in rgb colorspace), return 255
 
 def apply_watermark(fileName): # applies watermark pattern to entire image
     img = Image.open(fileName) # open passed filename
@@ -27,6 +28,7 @@ def apply_watermark(fileName): # applies watermark pattern to entire image
                 img.putpixel((x, y), (r, g, b)) # set pixel value to watermarked pixel value
                 #img.putpixel((x, y), (0, 0, 0)) # TODO: current test method blackens pixel, set values to tweak rgb values specifically
     img.show() # show image
+
 def main():
     fileExists = False
     while not fileExists:
@@ -38,7 +40,6 @@ def main():
             print(name + " not found.")
 
     apply_watermark(name)
-    
 
 if __name__ == "__main__":
     main()
