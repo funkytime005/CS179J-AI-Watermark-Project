8398b2578ca168faa51f75b716fe3a0d49f51fe1
augustpbennett <augustpbennennet@gmail.com>
2024-03-04 01:48:43 -0800

    revised apply_watermark() method (and associated helper methods) to match n-character binary string specification

--- apply.py
+++ apply.py
@@ -1,32 +1,72 @@
 import os
 from PIL import Image
 
-def set_value(channel): # sets individual color channel value within one pixel
-    return min((round(channel / 10) * 10), 255) # divides channel value by 10, rounds to nearest whole number, then appends 0. effectively rounds channel value to nearest 10.
-                                                # if rounded value > 255 (max value in rgb colorspace), return 255
+WATERMARK_BINARY = [1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0] # 15-digit watermark creates 0.0031% FP rate, can be changed arbitrarily
+
+def set_last_digit(r, i, complement): # if the current binary digit of the watermark is 0, red channel's value should be even. else, red channel's value should be odd
+    if not complement:
+        if WATERMARK_BINARY[i] == 0: # current digit is 0, make red channel even
+            # return 0 # temp test statement to make changes visibly obvious (when the whole point is to be invisible, it's difficult to ensure correctness visually)
+        
+            if r % 2 == 0: # red channel is already even -> do not alter
+                return r
+            else: # red channel is odd -> make even (note: if red channel is 255, adding 1 does not generate a valid rgb value so set to 254 instead)
+                return min(r + 1, 254)
+        else: # current digit is 1, make red channel odd
+            # return 255 # temp test statement to make changes visibly obvious (when the whole point is to be invisible, it's difficult to ensure correctness visually)
+        
+            if r % 2 == 0: # red channel is even -> make odd (note: highest even red is 254, so adding 1 will not overflow)
+                return r + 1
+            else: # red channel is already odd -> do not alter
+                return r
+    else: # COMPLEMENT CASE: same procedure, but exactly the opposite. this applies the complement of the binary watermark, protecting it against tampering by inversion
+        if WATERMARK_BINARY[i] == 1: # check opposite digit to exactly invert above branch
+            # return 0 # temp test statement to make changes visibly obvious (when the whole point is to be invisible, it's difficult to ensure correctness visually)
+        
+            if r % 2 == 0: 
+                return r
+            else: 
+                return min(r + 1, 254)
+        else: 
+            # return 255 # temp test statement to make changes visibly obvious (when the whole point is to be invisible, it's difficult to ensure correctness visually)
+        
+            if r % 2 == 0: 
+                return r + 1
+            else: 
+                return r
+
+def apply_timestamp(complement): # TODO: stub
+    pass
 
 def apply_watermark(fileName): # applies watermark pattern to entire image
     img = Image.open(fileName) # open passed filename
     _img = img.load()
     [width, height] = img.size
+    [x, y] = (int(width / 2), int(height / 2))
+    xtemp = x
 
-    for x in range(width): # parse over image
-        for y in range(height):
-            if (x % 10 == 0) and (y % 10 == 0): # watermark pattern: edit every 10th pixel on x, y (figure out something with unlikely FP rate)
-                [r, g, b] = _img[x, y] # get triplet of rgb values in current pixel
-                
-                r = set_value(r) # watermark pixel
-                g = set_value(g)
-                b = set_value(b)
-                
-                '''
-                print('red channel: ' + str(r) + '\n' + # test print: ensure that each color channel has had its last digit removed
-                      'green channel: ' + str(g) + '\n' + 
-                      'blue channel: ' + str(b) + '\n')
-                '''
-                
-                img.putpixel((x, y), (r, g, b)) # set pixel value to watermarked pixel value
-                #img.putpixel((x, y), (0, 0, 0)) # TODO: current test method blackens pixel, set values to tweak rgb values specifically
+    for i in range(len(WATERMARK_BINARY)): # apply invisible watermark
+        [r, g, b] = _img[xtemp, y]
+        r = set_last_digit(r, i, False)
+        
+        img.putpixel((xtemp, y), (r, g, b)) # draw marked pixel
+        xtemp += 1 # shift x position by 1
+
+    # TODO: APPLY UNIX TIMESTAMP IN BINARY TO END OF WATERMARK HERE
+    xtemp = x # reset temp position to center
+
+    ytemp = y + 10
+    for i in range(len(WATERMARK_BINARY)): # apply complement: if the watermark is inverted and reapplied, then the watermark cannot be destroyed if a bad actor adds 1 to every rgb value to "sanitize" the photo
+        [r, g, b] = _img[xtemp, ytemp]
+        r = set_last_digit(r, i, True)
+    
+        img.putpixel((xtemp, ytemp), (r, g, b)) # draw marked pixel
+        xtemp += 1 # shift x position by 1
+
+    # TODO: APPLY COMPLEMENT OF UNIX TIMESTAMP IN BINARY TO END OF WATERMARK HERE
+    xtemp = x # reset temp positions to center
+    ytemp = y
+
     img.show() # show image
 
 def main():
